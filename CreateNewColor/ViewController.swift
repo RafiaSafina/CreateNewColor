560392933d613471eaaa59f20748cbd70fd1ee99
rafia <safinarafia@gmail.com>
2022-09-24 00:17:00 +0300

    setup view again

--- CreateNewColor/ViewController.swift
+++ CreateNewColor/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //    MARK: - Outlets
+    
+    @IBOutlet var multiColoredView: UIView!
+    
+    @IBOutlet var redColorValue: UILabel!
+    @IBOutlet var greenColorValue: UILabel!
+    @IBOutlet var blueColorValue: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    //    MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        multiColoredView.layer.cornerRadius = 10
     }
 
-
+    //    MARK: - SlidersAction
+  
+    @IBAction func redSliderAction() {
+//        redColorValue.text = redSlider.value.formatted()
+    }
+    
+    @IBAction func greenSliderAction() {
+    }
+    
+    @IBAction func blueSliderAction() {
+    }
+    
 }
 
